3ff25a9150ea4b3ce47ec187683788e5cb4b500a
kalle (jag) <kalle.jillheden@me.com>
2018-01-30 01:24:31 +0100

    Change order delivered status

---
+++ NackademinUppgift07/Controllers/AdminOrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NackademinUppgift07.DataModels;
+
+namespace NackademinUppgift07.Controllers
+{
+	public partial class AdminController
+	{
+		public IActionResult ListOrders()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		//[ValidateAntiForgeryToken]
+		public async Task<IActionResult> ToggleOrderLevererad(int id)
+		{
+			Bestallning bestallning = await dbContext.Bestallning.FirstOrDefaultAsync(b => b.BestallningId == id);
+
+			if (bestallning == null)
+				return Json(new {success = false});
+
+			bestallning.Levererad = !bestallning.Levererad;
+			await dbContext.SaveChangesAsync();
+
+			return Json(new
+			{
+				success = true,
+				bestallning.BestallningId,
+				bestallning.Levererad,
+			});
+		}
+	}
+}
